85a7480df9778ebc846897c1af5da52b45a0f841
sonhwanju <hwanjuson@gmail.com>
2022-10-27 16:38:34 +0900

    도감에 버튼을 추가하고 onClick이벤트 추가

--- Assets/Scripts/UI/IllustratedGuidePrefab.cs
+++ Assets/Scripts/UI/IllustratedGuidePrefab.cs
@@ -9,10 +9,25 @@ public class IllustratedGuidePrefab : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI guideText;
 
+    private Button btn;
 
     private string text;
 
+    private void Awake()
+    {
+        btn = GetComponent<Button>();    
+    }
 
+    private void Start()
+    {
+        btn.onClick.AddListener(() =>
+        {
+            ((BaseTextPopup)PopupManager.Instance.OpenPopup("BaseText", false))
+            .SetHeaderText(guideText.text)
+            .SetMsgText(text)
+            .Open();
+        });
+    }
 
     public void SetGuideText(string msg, string text = null)
     {
