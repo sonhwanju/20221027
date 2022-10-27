6759bcb4b9c5436309c9a4b9211bc57c05879473
sonhwanju <hwanjuson@gmail.com>
2022-10-27 14:57:06 +0900

    도감 팝업 스크립트에서 파서를 이용해서 데이터를 불러와 프리팹들 생성하는 코드를 작성

--- Assets/Scripts/UI/Popup/IllustratedGuidePopup.cs
+++ Assets/Scripts/UI/Popup/IllustratedGuidePopup.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class IllustratedGuidePopup : TabPopup
 {
-    
+    [SerializeField]
+    private RectTransform[] contents;
+
+    [SerializeField]
+    private IllustratedGuidePrefab guidePrefab;
+
+    private GuideParser parser;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        parser = GetComponent<GuideParser>();
+
+        if (parser == null) return;
+
+        GuideData[] datas = parser.Parse("GuideFile");
+
+        for (int i = 0; i < datas.Length; i++)
+        {
+            GuideData data = datas[i];
+            for (int j = 0; j < data.names.Length; j++)
+            {
+                IllustratedGuidePrefab prefab = Instantiate(guidePrefab, contents[i]);
+                prefab.SetGuideText(data.names[j]);
+            }
+        }
+    }
 }
