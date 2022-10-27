42aae7ff893fdaad6b62a9d4501cdd996c467cf4
sonhwanju <hwanjuson@gmail.com>
2022-10-27 10:45:13 +0900

    DOTween 임포트 및 초기 세팅

--- Assets/Scripts/Manager/PopupManager.cs
+++ Assets/Scripts/Manager/PopupManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class PopupManager : MonoBehaviour
 {
-    
+    public static PopupManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("PopupManager가 여러 개 있습니다.");
+            return;
+        }
+        Instance = this;
+    }
 }
