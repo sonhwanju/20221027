2572d2829ba331bca006ee51e4d10712f274c6af
sonhwanju <hwanjuson@gmail.com>
2022-10-28 16:57:23 +0900

    AnimButton 스크립트가 Button을 상속받는게 아닌 OnClickSoundButton을 상속 받게 변경

--- Assets/Scripts/UI/AnimButton.cs
+++ Assets/Scripts/UI/AnimButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AnimButton : Button
+public class AnimButton : OnClickSoundButton
 {
     private PopupData popupData = null;
 
