bc41824a22eea591b038ef741b0ad67e109478f7
sonhwanju <hwanjuson@gmail.com>
2022-10-28 16:29:48 +0900

    SoundManager 스크립트 작성 (간단한 싱글톤만 작성)

---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Logger.LogError("SoundManager�� ������ ���� ��");
+            return;
+        }
+        Instance = this;
+    }
+}
