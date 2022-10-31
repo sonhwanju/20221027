54ff613973cb3ef5325758d84a2f0c4c7009181c
sonhwanju <hwanjuson@gmail.com>
2022-10-31 16:45:18 +0900

    상점 팝업을 열고 닫을 때 마다 자동스크롤을 켜고 끌 수 있도록 ShopPopup 스크립트를 수정

--- Assets/Scripts/UI/Popup/ShopPopup.cs
+++ Assets/Scripts/UI/Popup/ShopPopup.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ShopPopup : TabPopup
 {
-    
+    [SerializeField]
+    private AutoScroll autoScroll;
+
+    public override void Open()
+    {
+        base.Open();
+
+        autoScroll.StartScroll();
+    }
+
+    public override void Close()
+    {
+        autoScroll.StopScroll();
+
+        base.Close();
+    }
 }
