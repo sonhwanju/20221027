ffd359402d19192c3f04d6ac9510d32d869afae5
sonhwanju <hwanjuson@gmail.com>
2022-10-31 11:15:13 +0900

    TitleScene에 있는 TouchToStart 텍스트 FadeInOut을 위해 UtilClass에 Fade Coroutine 작성

--- Assets/Scripts/Util/UtilClass.cs
+++ Assets/Scripts/Util/UtilClass.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public static class UtilClass
 {
@@ -9,5 +11,26 @@ public static class UtilClass
         cg.alpha = on ? 1f : 0f;
         cg.interactable = on;
         cg.blocksRaycasts = on;
+    }
+
+    public static IEnumerator Fade<T>(T image, float fadeTime, bool fadeIn) where T : Graphic
+    {
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+        while (elapsedTime < fadeTime)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            if (fadeIn)
+            {
+                c.a = Mathf.Clamp01(elapsedTime / fadeTime);
+            }
+            else
+            {
+                c.a = 1f - Mathf.Clamp01(elapsedTime / fadeTime);
+            }
+
+            image.color = c;
+        }
     }
 }
