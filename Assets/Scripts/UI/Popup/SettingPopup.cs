db5e7745521a069d35193f8e2c5b41f92b8a7ad5
sonhwanju <hwanjuson@gmail.com>
2022-11-09 15:50:14 +0900

    SettingPopup 스크립트 수정
    게임 끌 시 데이터 저장, 세팅 팝업 생성 시 볼륨에 맞게 슬라이더 동기화

--- Assets/Scripts/UI/Popup/SettingPopup.cs
+++ Assets/Scripts/UI/Popup/SettingPopup.cs
@@ -10,4 +10,24 @@ public class SettingPopup : TabPopup
     [SerializeField]
     private VcaController sfxController;
 
+
+    protected override void Start()
+    {
+        base.Start();
+
+        SaveData data = DataManager.Instance.SaveData;
+
+        if (data != null)
+        {
+            bgmController.SetSlider(data.bgmVolume);
+            sfxController.SetSlider(data.sfxVolume);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        DataManager.Instance.SaveData = new SaveData()
+            .SetBgmVolume(bgmController.GetVolume())
+            .SetSfxVolume(sfxController.GetVolume());
+    }
 }
